e6be67f6e7e9fa39b3d2c6e2cc07e66be052c2b7
Waqar <waqaryou06@gmail.com>
2026-03-17 18:05:02 +0500

    Using 2D Arrays basic concepts

---
+++ Student.java
@@ -0,0 +1,77 @@
+
+  import java.util.Scanner;
+class GradeBook{
+
+      int[][] grades;
+    String [] subjectName;
+    String [] studentName;
+
+    
+
+    public GradeBook( String[] students, String[] subjects) {
+        this.grades = new int[students.length][subjects.length];
+        this.studentName = students;
+        this.subjectName = subjects;
+    }
+
+    void Add(int studentIndex,int subjectIndex,int num){
+        
+             grades[studentIndex][subjectIndex]=num;
+           
+           System.out.println("Grades:"+grades[studentIndex][subjectIndex]);
+       }
+       
+       
+       double getAverage(double mean){
+           
+           return mean/subjectName.length;
+           
+           
+           
+       }
+       
+       
+             
+}
+
+   public class Student{
+
+   public static void main(String[] args) {
+       int Grade;
+      
+       String students[]={"Waqar"};
+       
+       String subjects[]={"Math","Science"};
+
+         GradeBook gb=new GradeBook(students,subjects);
+              Scanner sc=new Scanner(System.in);
+        
+        for(int i=0; i<students.length; i++){
+        
+            for(int j=0; j<subjects.length; j++){
+                
+               System.out.println("Enter Student Grades:");
+                   Grade= sc.nextInt();
+                   
+                 gb.Add(i,j,Grade);  
+            }        
+                
+    }
+            double Average=0;
+            for(int i=0; i<1; i++){
+        
+            for(int j=0; j<subjects.length; j++){
+                
+                Average+=gb.grades[i][j];
+                
+            }
+          }    
+              System.out.println("Average Per Student:"+gb.getAverage(Average));
+            
+            
+            
+            
+            
+            
+        }
+   }
